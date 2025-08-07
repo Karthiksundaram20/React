2e930f1bd11ac06f5522eb6ef684f52e87a13b08
Karthik <123794537+Karthiksundaram20@users.noreply.github.com>
2025-08-07 19:27:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React framework
+          Learn React framework to create website
         </a>
       </header>
     </div>
